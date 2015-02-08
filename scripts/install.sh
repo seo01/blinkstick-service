31570d2f1ef97955946204d4756e007908626203
Simon Overell <soverell@gmail.com>
2015-02-08 16:06:21 +0000

    typos

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,14 +11,13 @@ else
     URL="$DIR/../example.json"
 fi
 
-CMD="$DIR/../src/light.py $URL"
+CMD="python $DIR/../src/light.py $URL"
 
 echo "1. sed \"s,<COMMAND>,${CMD},g\" $DIR/blinkstick-service-init.sh > /etc/init.d/blinkstick-service"
-sed "s,<COMMAND>,${CMD},g" $DIR/blinkstick-service-init.sh 
-#> /etc/init.d/blinkstick-service
+#sed "s,<COMMAND>,${CMD},g" $DIR/blinkstick-service-init.sh > /etc/init.d/blinkstick-service
 echo "2. touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log"
 #touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log
 echo "3. update-rc.d blinkstick-service defaults"
 #update-rc.d blinkstick-service defaults
 echo "4. service blinkstick-service start"
-#service "$NAME" start
+#service blinkstick-service start
