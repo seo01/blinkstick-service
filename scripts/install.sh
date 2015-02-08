e78c4b3ad31a13e0b8126749547c994f8eca1fae
Simon Overell <soverell@gmail.com>
2015-02-08 16:11:06 +0000

    updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,13 +11,15 @@ else
     URL="$DIR/../example.json"
 fi
 
-CMD="python $DIR/../src/light.py $URL"
+CMD="python $DIR/../src/light.py -f $URL"
 
 echo "1. sed \"s,<COMMAND>,${CMD},g\" $DIR/blinkstick-service-init.sh > /etc/init.d/blinkstick-service"
-#sed "s,<COMMAND>,${CMD},g" $DIR/blinkstick-service-init.sh > /etc/init.d/blinkstick-service
-echo "2. touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log"
-#touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log
-echo "3. update-rc.d blinkstick-service defaults"
-#update-rc.d blinkstick-service defaults
-echo "4. service blinkstick-service start"
-#service blinkstick-service start
+sed "s,<COMMAND>,${CMD},g" $DIR/blinkstick-service-init.sh > /etc/init.d/blinkstick-service
+echo "2. chmod 755 /etc/init.d/blinkstick-service"
+chmod 755 /etc/init.d/blinkstick-service
+echo "3. touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log"
+touch /var/log/blinkstick-service.log && chown pi /var/log/blinkstick-service.log
+echo "4. update-rc.d blinkstick-service defaults"
+update-rc.d blinkstick-service defaults
+echo "5. service blinkstick-service start"
+service blinkstick-service start
